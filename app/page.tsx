ba90645e3bd0f1cb57a83b2e92106dcdfa8f4ca5
Akshat Sachan <sachanakshat@Akshats-MacBook-Air.local>
2024-09-15 22:31:20 +0530

    Added theme, feedbackCard and feedback backend

--- app/page.tsx
+++ app/page.tsx
@@ -2,12 +2,14 @@ import Login from "./login/page"
 import SignupPage from "./signup/page"
 import CreateReview from "./createReview/page"
 import LandingComponent from "@/components/LandingComponent";
+import FeedbackCard from "@/components/FeedbackCard";
 
 export default function Home() {
   return (
     // <Login/>
-    <LandingComponent/>
+    // <LandingComponent/>
     // <SignupPage/>
     // <CreateReview/>
+    <FeedbackCard/>
   );
 }
